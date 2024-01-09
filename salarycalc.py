9150d26fbbb871de2b8e3920fe79a8fee492aef7
Evan Simmons <47922276+evansimm@users.noreply.github.com>
2024-01-09 15:53:14 -0500

    Update salary conversion logic

--- salarycalc.py
+++ salarycalc.py
@@ -1,29 +1,32 @@
 #!/usr/bin/env python3
-#Salary Calculator v2.0
+#Salary Calculator v2.1
 #Converts annual salary to a hourly rate and vice versa.
 
 #Print to ask for annual or hourly income and prompt for user input.
 print("Would you like to convert from Annual Income or Hourly Income?\nPress A for Annual or H for Hourly.")
 income_type = input()
 
-if income_type == "A" or income_type == "a":
-	print("Please enter annual rate: ")
-	annual_rate = input()
+# function to convert annual pay to hourly pay
+def convert_annual_to_hourly(annual_rate):
 	hourly_rate = float(annual_rate) /52 /40
-	limit_hr = round(hourly_rate,2)
-	biweekly_rate = limit_hr * 80
-	print("An annual salary of $" + str(annual_rate) + " comes to $" + str(limit_hr) + " an hour.\nYour bi-weekly gross pay would be $" + str(biweekly_rate) + ".")
-	quit()
+	biweekly_rate = hourly_rate * 80
+	return hourly_rate, biweekly_rate
 
-if income_type == "H" or income_type == "h":
-	print("Please enter hourly rate: ")
-	hourly_rate = input()
+# function to convert hourly pay to annual pay
+def convert_hourly_to_annual(hourly_rate):
 	annual_rate = float(hourly_rate) * 40 * 52
-	limit_ar = round(annual_rate,2)
-	biweekly_rate = limit_ar /12 /2
-	print("An hourly rate of $" + str(hourly_rate) + " comes to $" + str(limit_ar) + " annually.\nYour bi-weekly gross pay would be $" + str(biweekly_rate) + ".")
-	quit()
-	
+	biweekly_rate = annual_rate /12 /2
+	return annual_rate, biweekly_rate
+
+if income_type.lower() == "a":
+	annual_rate = input("Please enter annual rate: ")
+	hourly_rate, biweekly_rate = convert_annual_to_hourly(annual_rate)
+	print(f"An annual salary of ${annual_rate} comes to ${hourly_rate:.2f} an hour.\nYour bi-weekly gross pay would be ${biweekly_rate:.2f}.")
+
+elif income_type.lower() == "h":
+    hourly_rate = input("Please enter hourly rate: ")
+    annual_rate, biweekly_rate = convert_hourly_to_annual(hourly_rate)
+    print(f"An hourly rate of ${hourly_rate} comes to ${annual_rate:.2f} annually.\nYour bi-weekly gross pay would be ${biweekly_rate:.2f}.")
+
 else:
-	print("Invalid entry for Income Type. Please try again.")
-	quit()
+	print("Invalid entry. Please try again.")
